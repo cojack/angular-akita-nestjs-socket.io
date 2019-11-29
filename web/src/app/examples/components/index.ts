b035277f4ed22abb00c9889bf479688b325800d4
cojack <xcojack@gmail.com>
2019-11-29 22:50:20 +0100

    fix(examples): forgot to remove unnecessary export

--- web/src/app/examples/components/index.ts
+++ web/src/app/examples/components/index.ts
@@ -1,4 +1,3 @@
 export * from './dirty-check/dirty-check.component';
 export * from './todos/todo.component';
 export * from './persist-form/story-form.component';
-export * from './persist-form/stories.component';
